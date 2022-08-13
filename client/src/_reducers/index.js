196682943e07a1d66f17dac79281bd8f1edaf582
jeongabae <gabea52@gmail.com>
2022-08-13 23:52:18 +0900

    [create]

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+//import user from './user _reducer'
+
+const rootReducer = combineREducers({
+    //user,
+})
+
+export default rootReducer; //다른 파일에서 이 reducer를 쓸 수 있게 export
